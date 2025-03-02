6d5ea54f97a7a7f7ba0ec6931a63780fadc359a5
Edmdeg21 <122512735+Edmdeg21@users.noreply.github.com>
2025-03-01 20:09:05 -0500

    Update metasploit-scan.sh

--- projects/penetration-testing/metasploit-scan.sh
+++ projects/penetration-testing/metasploit-scan.sh
@@ -1,1 +1,20 @@
+---
 
+## 📜 2. metasploit-scan.sh (Bash Script)
+
+```bash
+#!/bin/bash
+
+# Automated Metasploit auxiliary scan script
+TARGET="192.168.1.100"
+
+msfconsole -q -x "
+use auxiliary/scanner/portscan/tcp;
+set RHOSTS $TARGET;
+set THREADS 10;
+run;
+use auxiliary/scanner/http/http_version;
+set RHOSTS $TARGET;
+run;
+exit
+"
